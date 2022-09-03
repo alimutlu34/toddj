f887163ad4dabad7e3da4b93deae5b1c99c727fc
alimutlu34 <ali.mutlu@ogr.iu.edu.tr>
2022-09-03 13:00:08 +0300

    last change

--- main/settings.py
+++ main/settings.py
@@ -15,7 +15,7 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-django_heroku.settings(locals())
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -125,3 +125,4 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
+django_heroku.settings(locals())
